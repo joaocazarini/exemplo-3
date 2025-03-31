dc8fb5e94f35116da060e5b8a79652b5109fcbd2
joao <jvcazarini@gmail.com>
2025-03-31 09:54:36 -0300

    operadores aritmeticos, logicos e comparação

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,84 @@
-//DECLARAÇÕES E VARIÁVEIS
+// //DECLARAÇÕES E VARIÁVEIS
 
-var nome ="fiap";
-console.log(nome);
+// var nome ="fiap";
+// console.log(nome);
 
-let idade = 19;
-console.log(idade);
+// let idade = 19;
+// console.log(idade);
 
-const sobrenome="fiapinho";
-console.log(sobrenome);
+// const sobrenome="fiapinho";
+// console.log(sobrenome);
 
-//declarando uma variável indefinida
-let nome1;
-console.log(nome1);
+// //declarando uma variável indefinida
+// let nome1;
+// console.log(nome1);
 
-//declando uma variavel nula
-let nome2=null;
-console.log(nome2)
+// //declando uma variavel nula
+// let nome2=null;
+// console.log(nome2)
 
-//TIPOS DE VARIÁVEIS
+// //TIPOS DE VARIÁVEIS
 
-let exemplo1=10;
-console.log(typeof exemplo1)
+// let exemplo1=10;
+// console.log(typeof exemplo1)
 
-let exemplo2 ="tecnologia";
-console.log(tyoeof exemplo2)
+// let exemplo2 ="tecnologia";
+// console.log(typeof exemplo2);
 
-let exemplo3=true;
-console.log(typeof exemplo3)
+// let exemplo3=true;
+// console.log(typeof exemplo3);
 
-let exemplo4{};
-console.log{}
+// let exemplo4 ={};
+// console.log ={}
+
+// let exemplo5 ={};
+// console.log(typeof exemplo5)
+
+// //CONVERSÕES
+
+// let m=numFloat =123.456;
+// console.log(parseInt(numFloat));
+
+// let numString ="123.456";
+// console.log(parsefloat(numstring));
+
+// let numFloat =123.411
+
+// //METODOS
+
+// //lenght = verifica o tamanho da string
+// let frase = "O mundo da tecnologia";
+// console.log(frase.length);
+
+// //indexOf -retorna um trechgo do texto
+// let texto="a programação indomável e sustentável";
+// comsole.log(texto.indexOf(""))
+
+// //slice - retorna um trecho do texto com ínicio e fim
+// let info="processamento de ponta";
+// console.log(info.slice(2,6))
+
+// //OPERADORES ARITMÉTICOS
+
+// const num1=10;
+// const num2=20;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+
+//OPERADORES LÓGICOS
+
+const num3=10;
+const num4=20;
+
+console.log(num3 < num4);
+console.log(num3> num4 && num4 < num3);
+console.log (num3 < num4 || num4 < num3);
+
+//operadores de comparação
+
+console.log(num3 = num4);
+console.log(num3 == num4);
+//console.log(num3 === num4);
