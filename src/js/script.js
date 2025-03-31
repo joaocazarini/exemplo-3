b11936377c38eda2d70af8f538aba08d5b77c252
joao <jvcazarini@gmail.com>
2025-03-31 09:08:05 -0300

    declarações e variáveis

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+//DECLARAÇÕES E VARIÁVEIS
+
+var nome ="fiap";
+console.log(nome);
+
+let idade = 19;
+console.log(idade);
+
+const sobrenome="fiapinho";
+console.log(sobrenome);
+
+//declarando uma variável indefinida
+let nome1;
+console.log(nome1);
+
+//declando uma variavel nula
+let nome2=null;
+console.log(nome2)
+
